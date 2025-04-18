bfd80ddbde4467da3cf8a927b9c9b4f33e19e0ae
LorenzoAm <lorenzoamoretti99@gmail.com>
2025-04-18 10:00:00 +0200

    Sistemato il main

--- src/HelloWorldMain.java
+++ src/HelloWorldMain.java
@@ -1,11 +1,8 @@
 public class HelloWorldMain{
 	public static void main(String args[]){
-<<<<<<< HEAD
 		System.out.println("Hello world!");
 		System.out.println("Hi again from branch main!");	
-=======
 		System.out.println("Hello world!");	
 		System.out.println("Hi from feature one!");
->>>>>>> feature1
 	}
 }
