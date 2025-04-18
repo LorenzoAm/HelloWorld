def874cb54c7de3b7f4e8c9b6dcfe1ab7205e7b1
LorenzoAm <lorenzoamoretti99@gmail.com>
2025-04-18 09:34:28 +0200

    Added feature on branch main

--- src/HelloWorldMain.java
+++ src/HelloWorldMain.java
@@ -1,5 +1,6 @@
 public class HelloWorldMain{
 	public static void main(String args[]){
-		System.out.println("Hello world!");	
+		System.out.println("Hello world!");
+		System.out.println("Hi again from branch main!");	
 	}
 }
