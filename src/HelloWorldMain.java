8085008f24e3d8d86eb4f7d2927b8ad88aaefa74
lorenzoamoretti <72865087+LorenzoAm@users.noreply.github.com>
2025-05-08 09:59:30 +0200

    Updated HelloWorldMain.java

--- src/HelloWorldMain.java
+++ src/HelloWorldMain.java
@@ -1,8 +1,5 @@
 public class HelloWorldMain{
 	public static void main(String args[]){
 		System.out.println("Hello world!");
-		System.out.println("Hi again from branch main!");	
-		System.out.println("Hello world!");	
-		System.out.println("Hi from feature one!");
 	}
 }
